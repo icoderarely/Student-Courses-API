3a74b258fcb0dc1032c976f80f947a8c3a03a29a
semicolon guy <itworksnavneet@gmail.com>
2025-08-24 16:36:46 +0530

    format: ruff formatted stuff for me, cause i changed from octal (which i didnt mean) to integer

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
 
 student_data = [
     {
-        "id": 01,
+        "id": 1,
         "name": "Alice",
         "age": 20,
         "courses": [
@@ -13,26 +14,68 @@ student_data = [
         ],
     },
     {
-        "id": 02,
+        "id": 2,
         "name": "Bob",
         "age": 22,
         "courses": [{"id": 103, "name": "Chemistry", "credits": 3}],
     },
 ]
 
+
 class Course(BaseModel):
-    id: Annotated[int, Field(..., description="Course ID to be entered (3 digits)", examples=[123, 104])]
-    name: Annotated[str, Field(..., description="Course name", examples=["Math", "Chemistry"])]
-    credits: Annotated[int, Field(..., description="Credits the course has to offer (usually single digit)", examples=[3, 5])]
+    id: Annotated[
+        int,
+        Field(
+            ..., description="Course ID to be entered (3 digits)", examples=[123, 104]
+        ),
+    ]
+    name: Annotated[
+        str, Field(..., description="Course name", examples=["Math", "Chemistry"])
+    ]
+    credits: Annotated[
+        int,
+        Field(
+            ...,
+            description="Credits the course has to offer (usually single digit)",
+            examples=[3, 5],
+        ),
+    ]
+
 
 class CourseUpdate(BaseModel):
-    id: Annotated[int | None, Field(description="Course ID to be entered (3 digits)", examples=[123, 104])] = None
-    name: Annotated[str | None, Field(description="Course name", examples=["Math", "Chemistry"])] = None
-    credits: Annotated[int | None, Field(description="Credits the course has to offer (usually single digit)", examples=[3, 5])] = None
+    id: Annotated[
+        int | None,
+        Field(description="Course ID to be entered (3 digits)", examples=[123, 104]),
+    ] = None
+    name: Annotated[
+        str | None, Field(description="Course name", examples=["Math", "Chemistry"])
+    ] = None
+    credits: Annotated[
+        int | None,
+        Field(
+            description="Credits the course has to offer (usually single digit)",
+            examples=[3, 5],
+        ),
+    ] = None
+
 
 class Student(BaseModel):
-    id: Annotated[int, Field(..., description="Id of student - 2 digits max", lt=100, examples=[1, 20])]
-    name: Annotated[str, Field(..., max_length=40, title="Student Name", description="Students full name to be stored in db", examples=["John Smith", "Joseph Killings"])]
+    id: Annotated[
+        int,
+        Field(
+            ..., description="Id of student - 2 digits max", lt=100, examples=[1, 20]
+        ),
+    ]
+    name: Annotated[
+        str,
+        Field(
+            ...,
+            max_length=40,
+            title="Student Name",
+            description="Students full name to be stored in db",
+            examples=["John Smith", "Joseph Killings"],
+        ),
+    ]
     age: Annotated[int, Field(..., gt=18)]
     courses: list[Course]
 
@@ -41,10 +84,30 @@ class Student(BaseModel):
     def total_credits(self) -> int:
         return sum(course.credits for course in self.courses)
 
+
 class StudentUpdate(BaseModel):
-    id: Annotated[int | None, Field(description="Id of student - 2 digits max", lt=100, examples=[1, 23])] = None
-    name: Annotated[str | None, Field(max_length=40, title="Student Name", description="Students full name to be stored in db", examples=["John Smith", "Joseph Killings"])] = None
+    id: Annotated[
+        int | None,
+        Field(description="Id of student - 2 digits max", lt=100, examples=[1, 23]),
+    ] = None
+    name: Annotated[
+        str | None,
+        Field(
+            max_length=40,
+            title="Student Name",
+            description="Students full name to be stored in db",
+            examples=["John Smith", "Joseph Killings"],
+        ),
+    ] = None
     age: Annotated[int | None, Field(..., gt=18)] = None
     courses: list[Course] | None = None
 
+
 app = FastAPI(title="Student Courses API")
+
+
+@app.get("/")
+def rootDir():
+    return JSONResponse(
+        status_code=200, content={"success": "server is working and is loaded"}
+    )
